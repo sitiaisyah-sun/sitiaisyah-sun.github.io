b2d85b59500bd9cf7d5afdd2618e0c6f05ec7db2
Siti Aisyah <imyourflower97@gmail.com>
2025-03-12 14:32:14 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function pesanSelamatDatang() {
+    console.log("Selamat datang di Perpustakaan Digital!");
+}
+
+function ubahJudul() {
+    document.querySelector("h1").innerHTML = "Judul Berubah!";
+}
